7a1f681eb5153a7ace3ddbfd18bd0df258831265
lucascaiafa00 <lucascaiafa00@gmail.com>
2024-02-06 20:58:14 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import './index.css'
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <HashRouter>
-    <App />
-  </HashRouter>
-</React.StrictMode>
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
